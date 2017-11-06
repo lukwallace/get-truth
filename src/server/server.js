d935c892346d06d01c793b30f73d0aab8e5aed28
Wallace Luk <wluk@forio.com>
2017-11-06 10:30:44 -0800

    init

---
+++ src/server/server.js
@@ -0,0 +1,28 @@
+const truths =require('./truth.json');
+const express = require('express');
+const app = express();
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+const SERVER_DELAY = 5000;
+app.get('/get-truth', (req, res) => {
+    const randomTruth = truths[Math.floor(Math.random() * truths.length)];
+    const randomDelay = Math.random() * 5000;
+    const failure = Math.random() < 0.3;
+
+    setTimeout(() => {
+        if (failure) {
+            res.status(503).end();
+            return;
+        }
+
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify({ truth: randomTruth }));
+    }, randomDelay);
+});
+
+app.listen(3000, () => console.log('Truth Server listening locally on port 3000!'));
