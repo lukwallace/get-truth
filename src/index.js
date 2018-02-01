e109880cf0167450da95bbb05d23e509e7c56689
Michael Mach <mikemach123@gmail.com>
2018-02-01 08:32:20 -0800

    style button and made screen wipe clear

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 const button = document.getElementById('button');
 
-button.addEventListener('click', () => {
+button.addEventListener('click', (e) => {
     const wipe = document.getElementById('wipe')
     wipe.className = "enlarge";
+    window.setTimeout(() => {
+        wipe.className = "clear"
+        window.setTimeout(() => {
+            wipe.className = "reset";
+            window.setTimeout(() => {
+                wipe.className = "";
+            }, 2000)
+        }, 2000);
+    }, 2000);
+    e.target.className = 'no-events';
+    window.setTimeout(() => {
+        e.target.className = '';
+    }, 6000);
 });
